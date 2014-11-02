5871b988be2c217ad7b9d2858d43852926e8431f
harrise <harrise@cmc304-06.mathcs.carleton.edu>
2014-11-02 14:13:04 -0600

    changes from last meeting

--- Parser.py
+++ Parser.py
@@ -29,16 +29,19 @@ class Stack(object):
 # Input: String.
 # Output: List of strings, empty if a syntax error has occurred.
 def tokenList(program):
-
+        
 		#tokenList = re.findall(r'[[\(]|[\)]|\'(?:[\s*\(*\)*\,*\.*\?*(\\\')*\w*\s*]*)\'|[^\'\s\(\)]+]*', program)
-		#tokenList = re.findall(r'[[a-zA-Z][a-zA-Z0-9]*[\w*|^a-zA-Z0-9\w]+]*', program) #FIX THE \W to be symbols!!
+	    #tokenList = re.findall(r'[[a-zA-Z][a-zA-Z0-9]*[\w*|^a-zA-Z0-9\w]+]*', program) #FIX THE \W to be symbols!!
+	    #regexp expects the first character to be a letter, followed by any series of alphanumeric characters, which is then followed by
+	    #1) one or more of: Any number of whitespace characters OR non-alphanumeric character followed by a whitespace character
+	    #2) zero or more of: any number of whitespace characters OR one letter followed by zero or more alpha-numeric characters
+	    #3) A single period or a comma
+		tokenList = re.findall(r'[[a-zA-Z][a-zA-Z0-9\_]*[[\w*|^a-zA-Z0-9\w]+|[\w*|[[a-zA-Z][a-zA-Z0-9\_]*]*|[\.\,]]*', program) #can we set these clauses to variables?
 
-		'''
-		tokenList = re.findall(r'[[\(]|[\)]|\'(?:[\s*\(*\)*\,*\.*\?*(\\\')*\w*\s*]*)\'|[^\'\s\(\)]+]*', program)
->>>>>>> b5aec0a435ad54df390f3f944d2af561d045487c
-		for i in range(len(tokenList)):
-			tokenList[i] = re.sub(r'(?:\\\')', '\'',  tokenList[i])
-		return tokenList
+	
+		#tokenList = re.findall(r'[[\(]|[\)]|\'(?:[\s*\(*\)*\,*\.*\?*(\\\')*\w*\s*]*)\'|[^\'\s\(\)]+]*', program)
+		#for i in range(len(tokenList)):
+			#tokenList[i] = re.sub(r'(?:\\\')', '\'',  tokenList[i])
 		'''
 		words = re.findall(r'(?ms)\W*(\w+)', program)
 		tokenList = []
@@ -52,6 +55,7 @@ def tokenList(program):
 			else:
 				token = {'item':item, 'type':'undef'}
 				tokenList.append(token)
+		'''
 		return tokenList
 
 # Puts everything in the tokenList into a Stack
@@ -74,7 +78,7 @@ def main():
 
 # For testing what is in the stack in a live environment
 	environment = {"set":"set", "fun":"fun", "+":"+", "*":"*"}
-	print "Welcome to So Super Language. Press Control-D to exit."
+	print "Welcome to Gooey. Press Control-D to exit."
 	while True:
 		try:
 			program = raw_input(":> ")
