dca39e33cd85f7afdc6b3f4e4e757d1326673a36
unknown <harrise@carleton.edu>
2014-11-06 10:11:27 -0600

    tkinter example

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 if __name__ == "__main__":
 	from ast import *
+	from interpreter import *
 	print("Input a valid 'make' statement")
 	program = input(":> ")
-	#program = "make Button b with color red, size large."
 	makeStmt = ast(program)
-	print(makeStmt)
-	print("Object Type: ", makeStmt.objType)
-	print("Object name: ", makeStmt.varname)
-	for item in makeStmt.attributes:
-		print ("Common attribute type: ", item.attrType)
-		print ("Common attribute value: ", item.attrValue)
+		
+	interpret(makeStmt)
