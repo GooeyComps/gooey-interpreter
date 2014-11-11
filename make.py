6f7fad4fb97139c44b4a0086da9cf88787e07511
colel <colel@carleton.edu>
2014-11-10 19:32:50 -0600

    regex up the wazoo:

--- make.py
+++ make.py
@@ -34,7 +34,37 @@ class Make(List):
 	grammar = attr("command", Command), blank, attr("objType", ObjType), blank, attr("varname", Var), attr("attributes",Attributes), "."
 '''
 
-def make():
+class make:
     name()
-	
+
+#Type, keyword, variable name
+class var:
+    grammar = re.compile('[a-zA-Z][a-zA-Z0-9\_]*')
+
+class floatingPoint:
+    grammar = re.compile('\d+\.{0,1}\d+')
+
+class integer:
+    grammar = re.compile('\d+')
+
+class period:
+    grammar = re.compile('\.')
+
+class symbols:
+    grammar = re.compile('[\=\+\-\/\*]')
+
+class comma:
+    grammar = re.compile('\,')
+
+class stringDouble:
+    grammar = re.compile('\"[a-zA-Z\s]+\"')
+
+class stringSingle:
+    grammar = re.compile('\'[a-zA-Z\s]+\'')
+
+class RGBtriplet:
+    grammar = re.compile('\(\d{1,3}\,\s*\d{1,3}\,\s*\d{1,3}\)')
+
+class HEX:
+    grammar = re.compile('\#[A-Fa-f0-9]{6}') 
 	
