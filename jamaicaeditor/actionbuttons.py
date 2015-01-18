e9cf4a3ec4b736c2e497bb594ba15800ae46d780
Leah E. Cole <leah.e.cole@gmail.com>
2015-01-18 11:28:17 -0600

    BUTTON ACTIONS BOOYAH

--- jamaicaeditor/actionbuttons.py
+++ jamaicaeditor/actionbuttons.py
@@ -1,19 +1,43 @@
 from tkinter import *
 
-class Actions:
 
-    # Print something actions
-    def write(printTest):
-        print(printTest)
+# class Actions:
 
-    # Close window actions
-    def close(window):
-        window.quit()
+# Print something actions
+#def write(printTest):
+def write(window, item):
+    printTest = item.action.text
+    print(printTest)
+    #return (window, item)
 
-    # Change color of window actions
-    def windowColorChange(window, c):
-        window.configure(bg= c)
-        print("here")
+# Close window actions
+#def close(window):
+def close(window, item):
+    window.quit()
+    #return (window, item)
+
+# Change color of window actions
+#def windowColorChange(window, c):
+def windowColorChange(window,item):
+    c = item.action.color
+    window.configure(bg= c)
+    #return (window,item)
+
+def findAction(item):
+    action = str(item.action.value)
+    #a = "%s(window,item)" % action
+    #print(a)
+    #a = "Actions.%s(window,item)" % action
+    a = "%s" % action
+    return a
+
+def callAction(window,item,action):
+
+    # print(locals())
+    # print(globals())
+    exec(action+"(window,item)")
+
+
 
     # Submit button
 
