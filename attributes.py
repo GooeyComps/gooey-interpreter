378ab24979536f189bc2dcef46ad66d1a2396b51
harriseh <harrise@carleton.edu>
2015-02-09 09:52:05 -0600

    menu options

--- attributes.py
+++ attributes.py
@@ -1,68 +1,100 @@
 from pypeg2 import *
 
-#Various regex to catch different value types
+#   SYMBOL DEFINITIONS: regex, Symbol, str, and Keyword
 rgbRegex = re.compile('\(\d{1,3}\,\s*\d{1,3}\,\s*\d{1,3}\)')
 intRegex = re.compile('\d+')
 hexRegex = re.compile('\#[A-Fa-f0-9]{6}')
 actionPrint = re.compile('"(.*?)"')
 optionsRegex = re.compile('\"(.+?)\"|\w+')
 textRegex = re.compile('[^"\n](.[^"]*)')
-#Colors
+
+class SizeGridValue(str):
+	grammar = "(", attr("columns", intRegex), ",", attr("rows", intRegex), ")"
+    
 class ColorKeywordValue(Keyword):
 	grammar = Enum(K("red"),K("blue"),K("green"))
-
-class ColorRGBValue(str):
-	grammar = rgbRegex
-
-class ColorHEXValue(str):
-	grammar = hexRegex
-
-#Positions
+    
+class SizeKeywordValue(Keyword):
+	grammar = Enum(K("small"),K("large"))
+    
 class PositionGridValue(str):
-	grammar = "(", intRegex, ",", intRegex, ")"
+	grammar = "(", attr("x", intRegex), ",", attr("y", intRegex), ")"
+    
+class Text(str):
+	grammar = "\"", word, "\""
+    
 
+    
 class PositionKeywordValue(Keyword):
 	grammar = Enum(K("bottom"),K("top"), K("middle"), K("left"), K("right"))
 
-class PositionAttribute:
-    grammar = "position", blank, maybe_some(blank), attr("value", [PositionGridValue,PositionKeywordValue])
 
+#   WINDOW
+class WindowColorAttribute(List):
+    grammar = 'color', blank, attr('value', [rgbRegex, hexRegex, ColorKeywordValue])
+    
+class WindowSizeAttribute(List):
+    grammar = 'size', blank, attr('value', [intRegex, SizeGridValue, SizeKeywordValue])
+    
+class WindowAttribute(List):
+    grammar = [attr('color', WindowColorAttribute), attr('size', WindowSizeAttribute)]
+    
+class WindowAttributeList(List):
+    grammar = csl(WindowAttribute)
+    
+    
+#   BUTTON
+class ButtonColorAttribute(List):
+    grammar = 'color', blank, attr('value', [rgbRegex, hexRegex, ColorKeywordValue])
+    
+class ButtonPositionAttribute(List):
+    grammar = 'position', blank, attr('value', [PositionKeywordValue, PositionGridValue])
+    
+class ButtonSizeAttribute(List):
+    grammar = 'size', blank, attr('value', [intRegex, SizeGridValue])
+    
+class ButtonAttribute(List):
+    grammar = [attr('color', ButtonColorAttribute), attr('size', ButtonSizeAttribute), attr('position', ButtonPositionAttribute)]
+    
+class ButtonAttributeList(List):
+    grammar = csl(ButtonAttribute)
+    
+    
+#   MENU
+class MenuOptionsAttribute(List):
+    grammar = 'options', blank, attr('value', maybe_some(word))
+    
+class MenuAttribute(List):
+    grammar = [attr('options', MenuOptionsAttribute)]
+    
+class MenuAttributeList(List):
+    grammar = csl(MenuAttribute)
+    
+    
+#   MENUITEM
+class MenuItemTerminal(str):
+	grammar = "\"", attr("text", word), "\"", ":", attr("action", word)
+	
+class MenuItemOptionsAttribute(List):
+    grammar = 'options', blank, attr('value', maybe_some([word, MenuItemTerminal]))
+    
+class MenuItemTextAttribute(List):
+    grammar = 'text', blank, attr('value', Text)
+    
+class MenuItemAttribute(List):
+    grammar = [attr('options', MenuItemOptionsAttribute), attr('text', MenuItemTextAttribute)]
+    
+class MenuItemAttributeList(List):
+    grammar = csl(MenuItemAttribute)
+    
+	
+#   TEXTBOX
+class TextBoxTextAttribute(List):
+    grammar = 'text', blank, attr('value', Text)
+    
+class TextBoxAttribute(List):
+    grammar = [attr('options', TextBoxTextAttribute)]
+    
+class TextBoxAttributeList(List):
+    grammar = csl(TextBoxAttribute)
 
-class ColorAttribute:
-	grammar = "color", blank, attr("value", [ColorRGBValue, ColorHEXValue, ColorKeywordValue])
-
-#Sizes
-class SizeKeywordValue(Keyword):
-	grammar = Enum(K("small"),K("large"))
-
-class SizeGridValue(str):
-	grammar = "(", intRegex, ",", intRegex, ")"
-
-class SizeIntValue(str):
-	grammar = intRegex
-
-class SizeAttribute:
-	grammar = "size", blank, attr("value", [SizeKeywordValue, SizeIntValue, SizeGridValue])
-
-#Parent window for a Button
-class WindowAttribute:
-	grammar = "window", blank, attr("value", name())
-
-#Text
-class TextAttribute:
-	grammar = "text", blank, "\"", attr("value", textRegex), "\""
-
-#Menu Options
-class MenuOptionsAttribute:
-	grammar = "options", blank, attr("options", maybe_some(optionsRegex))
-
-#Button action (name of a function):
-class ActionAttribute:
-	grammar = "action", blank, attr("value", word), optional(attr("text", actionPrint)), optional(attr("color", [ColorRGBValue, ColorHEXValue, ColorKeywordValue]))
-
-#Wrap as Attribute object and put into AttributeList
-class Attribute:
-	grammar = [attr("color", ColorAttribute), attr("size", SizeAttribute), attr("window",WindowAttribute), attr("text", TextAttribute), attr("action",ActionAttribute), attr("values",MenuOptionsAttribute), attr("position",PositionAttribute)]
-
-class AttributeList(List):
-	grammar = csl(Attribute)
--- statements.py
+++ statements.py
@@ -2,46 +2,61 @@ from pypeg2 import *
 from attributes import *
 #Starts with a lowercase letter, can only be one word long,
 #only contain letters, numbers, or underscore
+
 varnameRegex = re.compile('[a-z][A-Za-z\d\_]*')
-class VarName(str):
-	grammar = varnameRegex
 
-class MakeType(Keyword):
-	grammar = Enum(K("Button"), K("Window"), K("Menu"), K("MenuItem"), K("TextBox"))
+class MakeWindow(List):
+    grammar = ['make','Make'], blank, attr('type', 'Window'), blank, attr('varname', word), optional( 'with', attr('attributes',WindowAttributeList)), '.'
+    
+class MakeButton(List):
+    grammar = ['make','Make'], blank, attr('type', 'Button'), blank, attr('varname', word), optional( 'with', attr('attributes',ButtonAttributeList)), '.'
 
-class Make(List):
-	#grammar = "make", blank, attr("type", MakeType), blank, attr("varname", VarName), optional("with", attr("attributes",AttributeList)), "."
-	grammar = "make", blank, attr("type", MakeType), blank, attr("varname", VarName), optional("with", attr("attributes",AttributeList))
+class MakeMenu(List):
+    grammar = ['make','Make'], blank, attr('type', 'Menu'), blank, attr('varname', word), optional( 'with', attr('attributes',MenuAttributeList)), '.'
+
+class MakeMenuItem(List):
+    grammar = ['make','Make'], blank, attr('type', 'MenuItem'), blank, attr('varname', word), optional( 'with', attr('attributes',MenuItemAttributeList)), '.'
+
+class MakeTextBox(List):
+    grammar = ['make','Make'], blank, attr('type', 'TextBox'), blank, attr('varname', word), optional( 'with', attr('attributes',TextBoxAttributeList)), '.'
+
+class GooeyMake(List):
+	grammar = [MakeWindow, MakeButton, MakeMenu, MakeMenuItem, MakeTextBox]
+	
+	
+class SetWindow(List):
+    grammar = ['set','Set'], blank, attr('varname', word), blank, attr('attributes',WindowAttributeList), '.'
+    
+class SetButton(List):
+    grammar = ['set','Set'], blank, attr('varname', word), blank, attr('attributes',ButtonAttributeList), '.'
+
+class SetMenu(List):
+    grammar = ['set','Set'], blank, attr('varname', word), blank, attr('attributes',MenuAttributeList), '.'
+
+class SetMenuItem(List):
+    grammar = ['set','Set'], blank, attr('varname', word), blank, attr('attributes',MenuItemAttributeList), '.'
+
+class SetTextBox(List):
+    grammar = ['set','Set'], blank, attr('varname', word), blank, attr('attributes',TextBoxAttributeList), '.'
 
 class GooeySet(List):
-	#grammar = "set", blank, attr("varname", VarName), attr("attributes",AttributeList), "."
-
- 	grammar = "set", blank, attr("varname", VarName), attr("attributes",AttributeList)
-
+	grammar = [SetWindow, SetButton, SetMenu, SetMenuItem, SetTextBox]
+	
 class Line(List):
-	grammar = attr("lineAction", [Make, GooeySet]), ";", blank
-
+	grammar = attr("lineAction", [MakeWindow, MakeButton, MakeMenu, MakeMenuItem, MakeTextBox, SetWindow, SetButton, SetMenu, SetMenuItem, SetTextBox]), ";", blank
+	
 class Return(List):
 	grammar = "return", attr("param", optional(blank, word))
 
 class FunctionDefinition(List):
-	# grammar = "function", blank, attr("funcname", VarName), "(", attr("params", \
-	# csl(maybe_some(word))), ")", blank, "does", blank, attr("funcaction", csl(maybe_some(word))), ";", \
-	# blank, optional("returns", blank, word), "."
-	# grammar = "function", blank, attr("funcname", VarName), "(", attr("params", \
-	# csl(maybe_some(word))), ")", blank, "does", blank, attr("funcaction", csl(maybe_some(word))), "."
-	# grammar = "function", blank, attr("funcname", VarName), "(", attr("params", \
-	# csl(maybe_some(word))), ")", blank, "does", blank, attr("funcaction", [Make, GooeySet])
-	grammar = "function", blank, attr("funcname", VarName), "(", attr("params", \
+	grammar = "function", blank, attr("funcname", varnameRegex), "(", attr("params", \
 	csl(maybe_some(word))), ")", blank, "does", blank, attr("funcaction", csl(maybe_some(Line), blank, Return))
 
-
 class FunctionCall(List):
-    grammar = "run", blank, attr("funcname", VarName), "(", attr("params", csl(maybe_some(word))), ")"
-
-
-
-
+    grammar = "run", blank, attr("funcname", varnameRegex), "(", attr("params", csl(maybe_some(word))), ")"
+	
 class Program(List):
-	grammar = maybe_some([Make, GooeySet, FunctionDefinition, FunctionCall], ".")
-	#grammar = maybe_some([FunctionDefinition,FunctionCall,InstructionLine])
+	grammar = maybe_some([MakeWindow, MakeButton, MakeMenu, MakeMenuItem, MakeTextBox, SetWindow, SetButton, SetMenu, SetMenuItem, SetTextBox])
+    
+    
+
