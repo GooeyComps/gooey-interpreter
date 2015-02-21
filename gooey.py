ab9941ca61b54c11cce4b8dd56ab382c471b0cf0
Jamaica Lammi <lammij@carleton.edu>
2015-02-21 16:21:19 -0600

    SO MANY THINGS WORK NOW IT'S SO GREAT HOLY CRAP

--- gooey.py
+++ gooey.py
@@ -21,6 +21,7 @@ class GUIWindow():
         #What if each binding is a dictionary element that contains type and varname?
 
         self.bindings = dict()
+        self.winBinding = None #This is used to keep track of the binding linked to our master window
         self.is_open = False
 
     def openWindow(self):
@@ -31,21 +32,30 @@ class GUIWindow():
         self.is_open = False
         #self.window.destroy()
 
+#    def modify(self, ast):
+#                '''
+#        Takes string Gooey code, parses and interprets it, and updates the preview window.
+#        Throws SyntaxError if syntax is incorrect, or GooeyError if there is an interpreter error.
+#        '''
+#        #create new instance of interpeter class, passing a reference the live preview window
+#        i = Interpreter(self.window,self.winBinding,self.bindings)
+#
+#        (self.bindings, self.winBinding) = i.interpret(ast)
+#        del i
     def modify(self, gooeyCode):
         '''
         Takes string Gooey code, parses and interprets it, and updates the preview window.
         Throws SyntaxError if syntax is incorrect, or GooeyError if there is an interpreter error.
         '''
         #create new instance of interpeter class, passing a reference the live preview window
-        i = Interpreter(self.window)
+        i = Interpreter(self.window,self.winBinding,self.bindings)
 
         ### START EMILY CHANGES
         ast = parse(gooeyCode, Program)
         ### END EMILY CHANGES ###
 
-        self.bindings = i.interpret(ast, self.bindings)
+        (self.bindings,self.winBinding) = i.interpret(ast)
         del i
-
 '''
 Class for the text pad window where the user types their input
 '''
@@ -66,6 +76,7 @@ class TextPad():
         fm.add_command(label="Stop",command=self.stop_preview)
 
 
+### START EMILY CODE ###
         # Add a "definitions" text area so users can see what they've already input but cannot edit it
         self.definitionLabel = Label(text="Definitions")
         self.definitionLabel.pack()
@@ -77,15 +88,21 @@ class TextPad():
         # Editing area/text pad
         self.editLabel = Label(text="Enter Code Below")
         self.editLabel.pack()
+### END EMILY CODE ###
+
 
         #add textPad to root and open window
         self.textPad = ScrolledText(self.root, width=59, height=10)
 
+### START EMILY CODE ###
         # Add a border to the text pad
         self.textPad.configure(borderwidth=4, highlightbackground="black")
+### END EMILY CODE ###
+
 
         self.textPad.pack(padx=(12,0))
 
+### START EMILY CODE ###
         # Add run and stop buttons to the text pad
         self.runIcon = PhotoImage(file="runIcon.gif")
         self.stopIcon = PhotoImage(file="stopIcon.gif")
@@ -97,6 +114,8 @@ class TextPad():
         self.runButton = Button(self.root)
         self.runButton.configure(image=self.runIcon, command=self.update_preview)
         self.runButton.pack(side=RIGHT)
+### END EMILY CODE ###
+
 
         #This is where the GUIWindow class is first called - since GUIWindow
         #makes a live preview window when it is initialized, we hide it until we need it
@@ -107,6 +126,18 @@ class TextPad():
         self.root.mainloop()
 
     def update_preview(self):
+#        self.retrieve_input()
+#
+#### START EMILY CODE ###
+#        self.definitions.configure(state=NORMAL)
+#        self.definitions.insert(END, self.text)
+#        self.definitions.configure(state=DISABLED)
+#### END EMILY CODE ###
+#
+#        ast = parse(self.text, Program)
+#        self.preview.modify(ast)
+#        #After it takes in a command and does something it deletes all of the text from the textbox
+#        self.textPad.delete('1.0',END)
         self.retrieve_input()
 
         ### START EMILY CHANGES ###
@@ -115,10 +146,8 @@ class TextPad():
         # Update preview window, handle errors gracefully.
         try:
             self.preview.modify(self.text)
-        except SyntaxError:
-            ErrorPopup("Syntax Error")
-        except GooeyError:
-            pass
+        except SyntaxError as e:
+            ErrorPopup("Syntax Error in line "+str(e.lineno)+".")
         else:
             # If there was no error in the interpreter,
             # copy text in text pad to definitions box, and delete from text pad
@@ -127,7 +156,6 @@ class TextPad():
             self.definitions.configure(state=DISABLED)
             self.textPad.delete('1.0',END)
         ### END EMILY CHANGES ###
-
 
     def open_preview(self):
         if hasattr(self,"preview"):
@@ -151,26 +179,22 @@ class TextPad():
     def retrieve_input(self):
         self.text = self.textPad.get('1.0', END)
 
-
 def readFromFile(infile):
     # Create bindings for Gooey program
     bindings = dict()
-
+    winBinding = None
     # Make the preview window, and hide it until it is needed.
     preview = GUIWindow()
     preview.window.withdraw()
 
     # Parse, interpret, and execute the program
-    i = Interpreter(preview.window)
+    i = Interpreter(preview.window,winBinding,bindings)
     try:
         ast = parse(infile.read(), Program)
-        bindings = i.interpret(ast, bindings)
-    except SyntaxError:
-        ErrorPopup("Syntax Error")
-    except GooeyError:
-        pass
+        (bindings,winBindings) = i.interpret(ast)
+    except SyntaxError as e:
+        ErrorPopup("Syntax Error in line "+str(e.lineno)+".")
     preview.window.mainloop()
-
 
 if __name__ == "__main__":
     #Create the command line argument parser
@@ -186,3 +210,4 @@ if __name__ == "__main__":
     else:
         textpad = TextPad()
         textpad.run()
+
